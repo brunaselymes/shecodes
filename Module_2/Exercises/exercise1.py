bb1e8218b87964896bef7091bc18018bd8c45c95
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-07-09 16:42:45 +1000

    fix conversion

--- Module_2/Exercises/exercise1.py
+++ Module_2/Exercises/exercise1.py
@@ -2,20 +2,20 @@
 #ex 1
 number1 = input("Enter a number: ")
 number2 = input("Enter another number: ")
-print(F"{number1} + {number2} = " + number1 + number2)
+print(F"{number1} + {number2} = " , int(number1) + int(number2))
 
 
 #######################
 #ex 2
 number3 = input("Enter a number: ")
 number4 = input("Enter another number: ")
-print(F"{number3} * {number4} = " + number3 * number4)
+print(F"{number3} * {number4} = " , float(number3) * float(number4))
 
 
 #######################
 #ex 3
 kilometer = input("Enter a adistance in kilometers: ")
-print(F"{kilometer}K = {kilometer * 1000}m = " )
+print(F"{kilometer}K = {float(kilometer) * 1000}m" )
 
 
 #######################
