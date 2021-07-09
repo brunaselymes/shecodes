3884e8ec78a500313045d74509c8250d647af0b6
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-07-09 16:30:38 +1000

    add exercise 1 pyhton

---
+++ Module_2/Exercises/exercise1.py
@@ -0,0 +1,25 @@
+#######################
+#ex 1
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+print(F"{number1} + {number2} = " + number1 + number2)
+
+
+#######################
+#ex 2
+number3 = input("Enter a number: ")
+number4 = input("Enter another number: ")
+print(F"{number3} * {number4} = " + number3 * number4)
+
+
+#######################
+#ex 3
+kilometer = input("Enter a adistance in kilometers: ")
+print(F"{kilometer}K = {kilometer * 1000}m = " )
+
+
+#######################
+#ex 4
+name = input("What is your name? ")
+height = input("How tall are you? ")
+print(F"{name} is {height}cms tall." )
---
+++ Module_2/Lesson/vabiables_playground.py
@@ -0,0 +1,37 @@
+# #Vaviables
+# dog = "Jett"
+# print(dog)
+# print(type(dog))
+# asli = f"Hey, my dog's name is {dog}"
+# print (asli)
+# day = "Saturday"
+# message = f'Today is {day}'
+# print(message)
+# print(type(day))
+# month = "July"
+# message2 = F"Today is {day} and the month is {month}"
+# print(message2)
+# apples = 5
+# oranges = apples
+# print (oranges)
+
+#run distance in m
+# run1_dist = 1400
+# run2_dist = 1800
+
+# #Addition
+# total_distance = run1_dist + run2_dist
+# print(F"Distance total is {total_distance}")
+
+# #Float
+# run3_dist = 1.7
+# run4_dist = 1.35
+
+# total_distance = run3_dist + run4_dist
+# print(F"Distance total is {total_distance}")
+
+# var1 = "banana"
+# print(int(var1))
+
+answer = input("How are you? ")
+print(answer)
