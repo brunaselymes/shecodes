49ab872a3839059c9758ea68924760b4f0a2f3de
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-07-10 13:02:00 +1000

    saturday

---
+++ Module_2/Lesson/conditional_playground.py
@@ -0,0 +1,14 @@
+is_raining = False
+is_cold = True
+
+# print(is_raining)
+# print(type(is_raining))
+# print(is_cold)
+# print(type(is_raining))
+
+print(is_raining) #false
+print(not is_raining) #true
+print(is_raining or is_cold) #true
+print(is_raining and not is_cold)#false
+print(is_raining or not is_cold)#false
+print(not is_raining and not is_cold)#false
---
+++ Module_2/Lesson/lists_playground.py
@@ -0,0 +1,10 @@
+#print sublist of items in positions 2 through 3 [2] [3]
+#print the item in -3
+#change the value of the first item in the list
+
+chilli_wishlist = ["igloo", "chicken","donut toy", "cardboard box"]
+
+print(chilli_wishlist[2:4])
+print(chilli_wishlist[-3])
+chilli_wishlist[0] = "bottle"
+print(chilli_wishlist)
