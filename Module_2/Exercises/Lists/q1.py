bf1917b8d95622e5da1b355cb3eff57dd09afd85
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-07-13 12:28:28 +1000

    exercises list

---
+++ Module_2/Exercises/Lists/q1.py
@@ -0,0 +1,29 @@
+foods = [
+    "orange",
+    "apple",
+    "banana",
+    "strawberry",
+    "grape",
+    "blueberry",
+    ["carrot", "cauliflower", "pumpkin"],
+    "passionfruit",
+    "mango",
+    "kiwifruit"
+]
+# # 1. The first item in the list.
+# print(foods[0])
+
+# # # 2. The third item in the list.
+# print(foods[2])
+
+# # 3. The last item in the list.
+# print(foods[-1])
+
+# # 4. The first three items in the list.
+# print(foods[0:3])
+
+# 5. The last three items in the list.
+
+
+# # 6. The last item in the sublist.
+# print(foods[6][-1])
