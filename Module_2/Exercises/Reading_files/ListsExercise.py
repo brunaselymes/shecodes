1f0819dbd440a5c0c5e39f338648760ff05cc261
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-07-31 11:28:53 +1000

    keeping up to date

--- Module_2/Exercises/Reading_files/ListsExercise.py
+++ Module_2/Exercises/Reading_files/ListsExercise.py
@@ -60,15 +60,14 @@ with open("./data/colours_20.csv") as csv_file:
 #RAL 2008,247-094-037,#F75E25,Hellrotorange,Bright red orange,Orangé rouge clair,Rojo claro anaranjado,Rosso arancio chiaro,Licht roodoranje
 #Does this count as red or orange?
 
-slowest = 0
-fastest = 0
-
-with open("./data/galaxies.csv") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for line in csv_reader:
-        if int(line[1]) < slowest:
-            slowest = int(line[1])
-        if int(line[1]) > fastest:
-            fastest = int(line[1])
-print(f"Slowest galaxy has the min velocity of {slowest}")
-print(f"Fastest galaxy has the max velocity of {fastest}")
+with open('data/galaxies.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    slow = [None, float('inf')]    #ignores first. Variable value = infinity (huge) 
+    fast = [None, 0]   #ignores first. Variable value = 0 (low)
+    for i, row in enumerate(csv_reader):    #i means index (naming convention). Row is row.
+        if i != 0:  #this first 'if' in the loop ensures each value is checked when the loop iterates because no rows = 0.
+            if int(row[1]) < slow[1]: #this 'if' (within the first'if') targets velocity data in each row: e.g. row[1] for row (18,19349) gets (19349). If velocity value is less than current slow value, do this:
+                slow =  [row[0], int(row[1])]  #this line re-defines the slow var value: it's now galaxy ([0]) + the velocity value just stored (because it was found to be lower than the last one stored)
+            if int(row[1])> fast[1]: #this 'if' (within the first'if') says if velocity is greater than current fast value, do this:
+                fast = [row[0], int(row[1])] #this line re-defines the fast var value: it's now galaxy ([0]) + the velocity value just stored (because it was found to be faster than the last one stored)
+    print(f'Galaxy {slow[0]} has a min velocity of {slow[1]}km/sec.') #formatted print returns the new slow variable - galaxy + value
